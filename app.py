453e9239eb97b5ccc685a1762f443115f577290a
cod3bug <sarthakg203@gmail.com>
2022-05-19 01:17:00 +0530

    final fiasco

--- app.py
+++ app.py
@@ -1,8 +1,65 @@
 import streamlit as st
 st.set_page_config(layout="wide")
 import streamlit.components.v1 as components
-
+import os
 from PIL import Image
+import numpy as np
+from keras.models import load_model
+
+classes = { 0:'Speed limit (20km/h)',
+            1:'Speed limit (30km/h)',
+            2:'Speed limit (50km/h)',
+            3:'Speed limit (60km/h)',
+            4:'Speed limit (70km/h)',
+            5:'Speed limit (80km/h)',
+            6:'End of speed limit (80km/h)',
+            7:'Speed limit (100km/h)',
+            8:'Speed limit (120km/h)',
+            9:'No passing',
+            10:'No passing veh over 3.5 tons',
+            11:'Right-of-way at intersection',
+            12:'Priority road',
+            13:'Yield',
+            14:'Stop',
+            15:'No vehicles',
+            16:'Vehicle > 3.5 tons prohibited',
+            17:'No entry',
+            18:'General caution',
+            19:'Dangerous curve left',
+            20:'Dangerous curve right',
+            21:'Double curve',
+            22:'Bumpy road',
+            23:'Slippery road',
+            24:'Road narrows on the right',
+            25:'Road work',
+            26:'Traffic signals',
+            27:'Pedestrians',
+            28:'Children crossing',
+            29:'Bicycles crossing',
+            30:'Beware of ice/snow',
+            31:'Wild animals crossing',
+            32:'End speed + passing limits',
+            33:'Turn right ahead',
+            34:'Turn left ahead',
+            35:'Ahead only',
+            36:'Go straight or right',
+            37:'Go straight or left',
+            38:'Keep right',
+            39:'Keep left',
+            40:'Roundabout mandatory',
+            41:'End of no passing',
+            42:'End no passing vehicle > 3.5 tons' }
+
+def image_processing(img):
+    pass
+    model = load_model('temp\\model\\TSR.h5')
+    data=[]
+    image = img.resize((30,30))
+    data.append(np.array(image))
+    Xtest = np.array(data)
+    Ypred = model.predict_classes(Xtest)
+    return Ypred
+
 
 st.title('🚦Indian Traffic Sign Classification')
 components.html(
@@ -34,7 +91,11 @@ if uploaded_file is not None:
     with open(os.path.join("uploads/",uploaded_file.name),"wb") as f: 
         f.write(uploaded_file.getbuffer())
     image = Image.open("uploads/"+uploaded_file.name)
-    st.image(image, caption="The speed limit is 50kmph")
+    pred = image_processing(image)
+    s = [str(i) for i in pred]
+    a = int("".join(s))
+    result = "Predicted Traffic🚦Sign is: " +classes[a]
+    st.image(image, caption= result)
 
 components.html(
     """
@@ -240,7 +301,7 @@ h5{
       
               <!-- Team item -->
               <div class="col-xl-3 col-sm-6 mb-5">
-                  <div class="bg-black rounded shadow-sm py-5 px-4"><img src="https://media-exp1.licdn.com/dms/image/C4D03AQF6A1qRhN-3-g/profile-displayphoto-shrink_400_400/0/1650805187481?e=1656547200&v=beta&t=kc2VkDtDRwDhAhBVmzskU206UYzBzbtbGg5vPm9-rgE" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+                  <div class="bg-black rounded shadow-sm py-5 px-4"><img src="https://media-exp1.licdn.com/dms/image/C5603AQEhdnVlUyhX4g/profile-displayphoto-shrink_400_400/0/1651610938884?e=1658361600&v=beta&t=pZoYr5YDSxuj8ubyAQbpwkdNXyMOnFiC5FOfAIj9bYQ" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
                       <h5 class="mb-0">Sarthak Gupta</h5><span class="small text-uppercase text-muted">CSE/18/28</span>
                       
                   </div>
